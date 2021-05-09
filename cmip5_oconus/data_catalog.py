a7fe7770f1e12c56eaa63b055b79b219d39b048c
Naoki Mizukami <mizukami@ucar.edu>
2021-05-08 18:23:14 -0600

    Option to use rcp45 historical. Fix masking for resample. Change due to use of vim instead of notebook editor

--- cmip5_oconus/data_catalog.py
+++ cmip5_oconus/data_catalog.py
@@ -99,20 +99,20 @@ def _calc_dtr(ds):
 
 
 def resample_data(ds, freq='MS', region=None, chunks=None):
-    
+
     if region is None:
         raise NotImplementedError('No other regions are not provided')
-        
+
     ds1 = xr.open_dataset(DOMAIN[region])
     mask = ds1['mask'].where(ds1['mask']==1).notnull()
-    
+
     out = xr.Dataset()
     for name, da in ds.data_vars.items():
         if name in ['PRCP', 'EVAP', 'total_runoff', 'RUNOFF', 'BASEFLOW']:
-            out[name] = da.resample(time=freq).sum('time').where(mask)
+            out[name] = da.resample(time=freq).sum(skipna=False)#.where(mask)
         else:
             # TODO: weight by days in month, or sum over year
-            out[name] = da.resample(time=freq).mean('time').where(mask)
+            out[name] = da.resample(time=freq).mean(skipna=False)#.where(mask)
 
     if chunks is not None:
         out = out.persist().chunk(chunks)
@@ -121,13 +121,13 @@ def resample_data(ds, freq='MS', region=None, chunks=None):
 
 
 # Wrappers - start
-def load_monthly_historical_hydro_datasets(models=None, 
-                                           variables=DEFAULT_MON_HYDRO_VARS, 
+def load_monthly_historical_hydro_datasets(models=None,
+                                           variables=DEFAULT_MON_HYDRO_VARS,
                                            region=None,
                                            dataType='grid',
                                            **kwargs):
     print('load_monthly_historical_hydro_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
@@ -146,12 +146,12 @@ def load_daily_historical_hydro_datasets(models=None,
                                          dataType='grid',
                                          **kwargs):
     print('load_daily_historical_hydro_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
         raise NotImplementedError('No other data types are supported at this time')
-        
+
     data = {}
     data['gcm'] = load_daily_cmip5_hydro_datasets('historical', models=models, variables=variables, region=region, dataType=dataType, **kwargs)
     data['obs'] = load_daily_obs_hydro_datasets(variables=variables, region=region, dataType=dataType, **kwargs)
@@ -169,12 +169,12 @@ def load_monthly_obs_hydro_datasets(variables=DEFAULT_MON_HYDRO_VARS,
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_obs_dataset(OBS_VIC_MON_ROOT_DIR[dataType][region], variables=variables, **kwargs)
-    
+
     # TODO: it would be better if we passed this info to the individual loaders
     if 'total_runoff' in variables:
         data['total_runoff'] = _calc_total_runoff(data)
     data = data[variables]
-    return data    
+    return data
 
 
 def load_daily_obs_hydro_datasets(variables=DEFAULT_DAY_HYDRO_VARS,
@@ -187,16 +187,16 @@ def load_daily_obs_hydro_datasets(variables=DEFAULT_DAY_HYDRO_VARS,
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_obs_dataset(OBS_VIC_DAILY_ROOT_DIR[dataType][region], variables=variables, **kwargs)
-    
+
     # TODO: it would be better if we passed this info to the individual loaders
     if 'total_runoff' in variables:
         data['total_runoff'] = _calc_total_runoff(data)
     data = data[variables]
-    return data   
+    return data
 
 
-def load_monthly_cmip5_hydro_datasets(scen, models=None, 
-                                      variables=DEFAULT_MON_HYDRO_VARS, 
+def load_monthly_cmip5_hydro_datasets(scen, models=None,
+                                      variables=DEFAULT_MON_HYDRO_VARS,
                                       region=None,
                                       dataType='grid',
                                       **kwargs):
@@ -208,30 +208,30 @@ def load_monthly_cmip5_hydro_datasets(scen, models=None,
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_cmip5_dataset(CMIP5_VIC_MON_ROOT_DIR[dataType][region], variables=variables, scen=scen, models=models, **kwargs)
-    
+
     # TODO: it would be better if we passed this info to the individual loaders
     if 'total_runoff' in variables:
         data['total_runoff'] = _calc_total_runoff(data)
-    
+
     data = data[variables]
-    
+
     return data
 
 
 def load_daily_cmip5_hydro_datasets(scen, models=None,
-                                    variables=DEFAULT_DAY_HYDRO_VARS, 
-                                    region=None, 
+                                    variables=DEFAULT_DAY_HYDRO_VARS,
+                                    region=None,
                                     dataType='grid',
                                     **kwargs):
     print('load_daily_cmip5_hydro_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_cmip5_dataset(CMIP5_VIC_DAY_ROOT_DIR[dataType][region], variables=variables, scen=scen, models=models, **kwargs)
-    
+
     # TODO: it would be better if we passed this info to the individual loaders
     if 'total_runoff' in variables:
         data['total_runoff'] = _calc_total_runoff(data)
@@ -240,13 +240,13 @@ def load_daily_cmip5_hydro_datasets(scen, models=None,
 
 
 ## Meteorology data
-def load_monthly_historical_met_datasets(models=None, 
-                                         variables=DEFAULT_MON_MET_VARS, 
+def load_monthly_historical_met_datasets(models=None,
+                                         variables=DEFAULT_MON_MET_VARS,
                                          region=None,
                                          dataType='grid',
                                          **kwargs):
     print('load_monthly_historical_met_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
@@ -265,12 +265,12 @@ def load_daily_historical_met_datasets(models=None,
                                        dataType='grid',
                                        **kwargs):
     print('load_daily_historical_met_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
         raise NotImplementedError('No other data types are supported at this time')
-        
+
     data = {}
     data['gcm'] = load_daily_cmip5_met_datasets('historical', models=models, variables=variables, region=region, dataType=dataType, **kwargs)
     data['obs'] = load_daily_obs_met_datasets(variables=variables, region=region, dataType=dataType, **kwargs)
@@ -288,20 +288,20 @@ def load_monthly_obs_met_datasets(variables=DEFAULT_MON_MET_VARS,
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_obs_dataset(OBS_MET_MON_ROOT_DIR[dataType][region], variables=variables, **kwargs)
-    
+
     if 'tmean' in variables:
         data['tmean'] = _calc_t_mean(data)
-        
+
     if 'dtr' in variables:
         data['dtr'] = _calc_dtr(data)
-          
+
     data = data[variables]
-    
-    return data 
+
+    return data
 
 
-def load_monthly_cmip5_met_datasets(scen, models=None, 
-                                    variables=DEFAULT_MON_MET_VARS, 
+def load_monthly_cmip5_met_datasets(scen, models=None,
+                                    variables=DEFAULT_MON_MET_VARS,
                                     region=None,
                                     dataType='grid',
                                     **kwargs):
@@ -313,40 +313,40 @@ def load_monthly_cmip5_met_datasets(scen, models=None,
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_cmip5_dataset(CMIP5_MET_MON_ROOT_DIR[dataType][region], variables=variables, scen=scen, models=models, **kwargs)
-    
+
     if 'tmean' in variables:
         data['tmean'] = _calc_t_mean(data)
-        
+
     if 'dtr' in variables:
         data['dtr'] = _calc_dtr(data)
-    
+
     data = data[variables]
-    
+
     return data
 
 
 def load_daily_cmip5_met_datasets(scen, models=None,
-                                  variables=DEFAULT_DAY_MET_VARS, 
-                                  region=None, 
+                                  variables=DEFAULT_DAY_MET_VARS,
+                                  region=None,
                                   dataType='grid',
                                   **kwargs):
     print('load_daily_cmip5_met_datasets', flush=True)
-    
+
     if region not in ['AK','HI']:
         raise NotImplementedError('No other regions are supported at this time')
     if dataType not in ['grid','hrus']:
         raise NotImplementedError('No other data types are supported at this time')
 
     data = load_cmip5_dataset(CMIP5_MET_DAY_ROOT_DIR[dataType][region], variables=variables, scen=scen, models=models, **kwargs)
-    
+
     if 'tmean' in variables:
         data['tmean'] = _calc_t_mean(data)
-        
+
     if 'dtr' in variables:
         data['dtr'] = _calc_dtr(data)
-        
+
     data = dat[variables]
-    
+
     return data
 
 # Wrappers - end
@@ -381,34 +381,36 @@ def filter_files(files, valid_years):
 
 def load_cmip5_dataset(root, variables=None, scen='rcp85', models=None, **kwargs):
     print('load_cmip5_dataset', flush=True)
-    
+
     if variables is None:
         raise NoneError("variables list not provided. Cannot access a 'None'.")
-            
+
     valid_years = get_valid_years(scen)
     if 'hist' in scen:
         scen = 'rcp85'  # bcsd put historical in the rcp dataset
+    elif 'hist_rcp45':
+        scen = 'rcp45'  # bcsd put historical in the rcp dataset
 
     if models is None:
         models = os.listdir(root)
         models.sort()
-    
+
     _file_tags = []
     for var in variables:
         _file_tags.append(FILE_TAG[var])
     file_tags = list(set(_file_tags))
-    
+
     ds_list = []
     models_list = []
     for m in progress(models):
-        
+
         for ix, tag in enumerate(file_tags):
             fpath = os.path.join(root, f'{m}', scen, f'*_{tag}_*nc')
             if ix==0:
                 files = glob.glob(fpath)
             else:
                 files += glob.glob(fpath)
-        
+
         if not files:
             warnings.warn('no files to open: %s' % fpath)
             models.remove(m)
@@ -433,29 +435,29 @@ def load_cmip5_dataset(root, variables=None, scen='rcp85', models=None, **kwargs
 
 def load_obs_dataset(root, variables=None, **kwargs):
     print('load_obs_dataset', flush=True)
-        
+
     if variables is None:
         raise NoneError("variables list not provided. Cannot access a 'None'.")
-    
+
     _file_tags = []
     for var in variables:
         _file_tags.append(FILE_TAG[var])
     file_tags = list(set(_file_tags))
-    
+
     files = []
     for tag in file_tags:
         fpath = os.path.join(root, f'*_{tag}_*nc')
         files.append(glob.glob(fpath))
-    
+
     ds = xr.open_mfdataset(files, **kwargs)
-    
+
     return ds
 
 
 ###. Not used
 def load_daily_obs_meteorology(region=None, dataType=None, **kwargs):
     print('load_daily_obs_meteorology', flush=True)
-    
+
     def preproc(ds):
         if 'latitude' in ds:
             # 1 or 2 files have different coordinate data so we fix that here
@@ -466,7 +468,7 @@ def load_daily_obs_meteorology(region=None, dataType=None, **kwargs):
                 ds = ds.drop(var)
         ds['lon'] = ds['lon'].where(ds['lon'] <= 180, ds['lon'] - 360)
         return ds
-    
+
     fpath = os.path.join(LIVNEH_MET_ROOT_DIR, '*nc')
     ds = xr.open_mfdataset(fpath, **kwargs)
     if 'longitude' in ds.coords:
@@ -475,11 +477,3 @@ def load_daily_obs_meteorology(region=None, dataType=None, **kwargs):
         ds = ds.rename({'Prec': 'pcp', 'Tmin': 't_min', 'Tmax': 't_max'})
     ds['t_mean'] = _calc_t_mean(ds)
     return ds[['t_mean', 'pcp']]
-
-
-def load_monthly_obs_meteorology(region=None, dataType=None, **kwargs):
-    print('load_monthly_daymet_meteorology', flush=True)
-    # for now, just load daily and resample imediately
-    ds = load_daily_obs_meteorology(region=None, dataType=None, **kwargs)
-    ds = resample_daily_data(ds)
-    return ds
